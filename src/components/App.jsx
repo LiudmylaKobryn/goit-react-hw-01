7de303ee727deb73e23786274d0ffd49d74b528e
LiudmylaKobryn <133100987+LiudmylaKobryn@users.noreply.github.com>
2024-07-01 20:52:13 +0200

    Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import Profile from "./Profile/Profile";
 import userData from "../userData.json";
 import FriendList from "./FriendList/FriendList";
-import friends from "/src/friends.json";
+import friends from "../friends.json";
+import transactions from "../transactions.json";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
 
 const App = () => {
   return (
@@ -14,6 +16,7 @@ const App = () => {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
